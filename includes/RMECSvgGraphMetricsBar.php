00596b1c2ff5b3e02821c5404b3671ea4caaa7b2
Ryan Eberly <40327564+gryan337@users.noreply.github.com>
2026-04-21 15:36:04 -0400

    Update RMECSvgGraphMetricsBar.php

--- includes/RMECSvgGraphMetricsBar.php
+++ includes/RMECSvgGraphMetricsBar.php
@@ -12,25 +12,22 @@ class RMECSvgGraphMetricsBar extends CSvgGroup {
 	private const ZBX_STYLE_CLASS = 'svg-graph-bar';
 
 	private $path;
+	private $metric;
 	private $itemid;
 	private $item_name;
 	private $options;
 	private $units;
 	private $data_set;
-	private $data_min;
-	private $data_max;
 
 	public function __construct(array $path, array $metric) {
 		parent::__construct();
 
 		$this->path = $path;
-
+		$this->metric = $metric ? : [];
 		$this->itemid = $metric['itemid'];
 		$this->item_name = $metric['name'];
 		$this->units = $metric['units'];
 		$this->data_set = $metric['data_set'];
-		$this->data_min = min(array_column($metric['points'], 'min'));
-		$this->data_max = max(array_column($metric['points'], 'max'));
 
 		$this->options = $metric['options'] + [
 			'color' => RMECSvgGraph::SVG_GRAPH_DEFAULT_COLOR,
@@ -72,14 +69,33 @@ class RMECSvgGraphMetricsBar extends CSvgGroup {
 	}
 
 	public function toString($destroy = true): string {
+		switch ($this->options['approximation']) {
+			case APPROXIMATION_AVG:
+				$max = max(array_column($this->metric['points'], 'avg'));
+				$min = min(array_column($this->metric['points'], 'avg'));
+				break;
+			case APPROXIMATION_MIN:
+				$max = max(array_column($this->metric['points'], 'min'));
+				$min = min(array_column($this->metric['points'], 'min'));
+				break;
+			case APPROXIMATION_MAX:
+				$max = max(array_column($this->metric['points'], 'max'));
+				$min = min(array_column($this->metric['points'], 'max'));
+				break;
+			default:
+				$max = max(array_column($this->metric['points'], 'max'));
+				$min = min(array_column($this->metric['points'], 'min'));
+				break;
+		}
+		
 		$this->setAttribute('data-set', 'bar')
 			->setAttribute('data-metric', $this->item_name)
 			->setAttribute('data-color', $this->options['color'])
 			->setAttribute('data-units', $this->units)
 			->setAttribute('data-index', $this->data_set)
 			->setAttribute('data-axisy', $this->options['axisy'])
-			->setAttribute('data-max', $this->data_max)
-			->setAttribute('data-min', $this->data_min)
+			->setAttribute('data-max', $max)
+			->setAttribute('data-min', $min)
 			->draw();
 
 		return parent::toString($destroy);
